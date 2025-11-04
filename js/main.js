3f739564be38399f3dc4e2eecc0f35688070c041
’Kenta-Iryo’ <k.iryou27@gmail.com>
2025-11-04 22:27:22 +0900

    2025.11.2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+  $('.fv__slider').slick({
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 6000,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
