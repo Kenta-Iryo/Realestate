dccf7f8c5871e3e3ffe09341bde1fd9298e02eab
’Kenta-Iryo’ <k.iryou27@gmail.com>
2025-12-07 22:56:36 +0900

    2025.12.7

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ $(document).ready(function () {
     cssEase: 'linear'
   });
 });
+
+$(".hoge-item .img-box > img").click(function () {
+  var src = $(this).attr("src");
+  var cap = $(this).attr("alt");
+  $(".modal-block").fadeIn().css("display", "flex");
+
+  $("#popup").attr("src", src);
+  $(".caption").text(cap);
+});
+
+$(".modal-block").click(function (e) {
+  // モーダルの外側をクリックした場合にのみモーダルを閉じる
+  if (e.target === this) {
+    $(this).css("display", "none");
+  }
+});
+
+$(".img-section > span").click(function () {
+  // モーダルを閉じるボタンがクリックされた場合にモーダルを閉じる
+  $(".modal-block").css("display", "none");
+});
